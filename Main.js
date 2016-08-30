82a434864e40fdeb7b407669ea5b9699ec2bb43b
Toncheff <toncheff96@abv.bg>
2016-08-30 17:17:38 +0300

    Show/Hide Login form

--- Main.js
+++ Main.js
@@ -4,3 +4,11 @@
    jQuery('form').animate({height: "toggle", opacity: "toggle"}, "50");
  });
 });
+
+function showLogin(){
+    document.getElementById("loginPage").style.display="block";
+}
+
+function hideLogin(){
+    document.getElementById("loginPage").style.display="none";
+}
