d4bae6a372aa28b4ab01bba6634f2ea501931941
Toncheff <toncheff96@abv.bg>
2016-08-31 20:32:05 +0300

    login/logout

--- Main.js
+++ Main.js
@@ -19,8 +19,50 @@ const kinveyBaseUrl = "https://baas.kinvey.com/";
 const kinveyAppKey = "kid_B1Vekw7j";
 const kinveyAppSecret = "5e93a0efbede4706b676ffe92176eee7";
 
+function showView(viewName) {
+    $('main > section').hide();
+    $('#' + viewName).show();
+}
+
+
+function showHideMenuLinks(){
+   $("#home").show();
+    if (sessionStorage.getItem('authToken') == null){
+
+        $("#postnow").hide();
+        $("#loginbutton").show();
+        $("#logoutbutton").hide();
+       /* $("#loginpage").hide();*/
+    } else {
+        $("#loginbutton").hide();
+        $("#logoutbutton").show();
+         $("#postnow").show();
+        /*  $("#loginpage").hide();*/
+    }
+    }
+
+$(function(){
+    showHideMenuLinks();
+    showView('seeHome')
+})
+
+$("#home").click(showshowHome);
+$("#recent").click(showshowRecent);
+$("#postnow").click(showshowPost);
+
+
 $('#login-form').submit(function(e) { e.preventDefault(); login();});
 $('#register-form').submit(function(e) { e.preventDefault(); register();});
+
+function showshowHome(){
+    showView('seeHome');
+}
+function showshowRecent(){
+    showView('seeRecent');
+}
+function showshowPost(){
+    showView('seePost');
+}
 
 
 function login(){
@@ -40,12 +82,29 @@ function login(){
         success: loginSuccess,
         error: handleAjaxError
     });
+        function loginSuccess(response){
+        let userAuth = response._kmd.authtoken;
+        sessionStorage.setItem('authToken', userAuth);
+        showHideMenuLinks();
+        
+    /*    showInfo('Login successful.');*/
+    }
+
+}
+
+function handleAjaxError(response){
+    let errorMsg = JSON.stringify(response);
+    if (response.readyState === 0)
+        errorMsg = "Cannot connect due to network error.";
+    if (response.responseJSON && response.responseJSON.description)
+        errorMsg = response.responseJSON.description;
+    //showError(errorMsg);
 }
 
 function register() {
     const kinveyRegisterUrl = kinveyBaseUrl + "user/" + kinveyAppKey + "/";
     const kinveyAuthHeaders = {
-        'Authorization' : "Basic" + btoa(kinveyAppKey + ":" + kinveyAppSecret),    
+        'Authorization' : "Basic " + btoa(kinveyAppKey + ":" + kinveyAppSecret),    
     };
     let userData = {
         username: $('#registerUser').val(),
@@ -56,28 +115,23 @@ function register() {
         url: kinveyRegisterUrl,
         headers: kinveyAuthHeaders,
         data: userData,
-        success: loginSuccess,
+        success: registerSuccess,
         error: handleAjaxError
     });
     
-    function loginSuccess(response){
-        let userAuth = response._kmd.authtoken;
+    function registerSuccess(response) {
+        let userAuth = response._kmd.authToken;
         sessionStorage.setItem('authToken', userAuth);
         showHideMenuLinks();
-        
-        showInfo('Login successful.');
+        //showInfo('User registration successful.');
     }
+    
+    
 }
-function showHideMenuLinks(){
-    $("#home").show();
-    if (sessionStorage.getItem('authToken') == null){
-     
-        $("#postnow").hide();
-        $("#loginbut").show();
-        $("#logoutbut").hide();
-    } else {
-        $("#loginbut").hide();
-        $("#logoutbut").show();
-         $("#postnow").show();
-    }
-    }
+
+function logout() {
+    sessionStorage.clear();
+    showHideMenuLinks();
+    showView('seeHome')
+
+}
